0723e127af02e43b33e0bceda7dac3829995f0ee
Ruan Linos Alves <ruan.compras@hotmail.com>
2020-03-28 11:46:09 -0300

    feat: implemented a first route to post a new ong

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get("/users/:id", (req, res) => {
-  const params = req.query;
+routes.post('/ongs', (request, response) => {
+  const data = request.body;
+  
+  console.log(data);
+  
   return res.json({
     message: "Hello World",
     id: params
   });
+
 });
 
 module.exports = routes;
