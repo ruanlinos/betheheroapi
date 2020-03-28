177841da0afca9429161b0c0386c74b3b6c0f762
Ruan Linos Alves <ruan.compras@hotmail.com>
2020-03-28 12:51:40 -0300

    fix: fixed some bugs and bad requests

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
+
 const routes = express.Router();
 
-routes.post('/ongs', (request, response) => {
-  const data = request.body;
-  
-  console.log(data);
-  
-  return res.json({
-    message: "Hello World",
-    id: params
-  });
+routes.post('/ongs', async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString('HEX');
+
+  await connection('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf,
+  })
+
+  return response.json({ id });
 
 });
 
